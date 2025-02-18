5b7228be54c994474b543299237efbf898914b8d
Clarus23 <dnjs3023@gmail.com>
2025-02-18 18:49:00 +0900

    Auditing 을 위한 annotation 추가

--- src/main/java/com/juc/homepage/HomepageApplication.java
+++ src/main/java/com/juc/homepage/HomepageApplication.java
@@ -2,7 +2,9 @@ package com.juc.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Spring JPA Auditing 을 하기 위한 annotation
 @SpringBootApplication
 public class HomepageApplication {
 
